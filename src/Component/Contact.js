bc56d50619bc0a10d89852bf2a33866f86121801
harismuda <harismuda20102003@gmail.com>
2023-11-21 12:43:28 +0700

    React Portofolio

---
+++ src/Component/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+        <h1>Hallo ini halaman Contact</h1>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/Component/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+        <h1>Hallo ini halaman Home</h1>
+    </div>
+  );
+}
+
+export default Home;
